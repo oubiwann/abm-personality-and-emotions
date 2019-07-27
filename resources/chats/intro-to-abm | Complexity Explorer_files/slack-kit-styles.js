95194c6ff7a82d549fe8cdac4ddae5d28d7f8435
Duncan McGreggor <oubiwann@gmail.com>
2019-07-27 13:02:33 -0500

    Added Slack chat about JVM, TDD, and software eng.

---
+++ resources/chats/intro-to-abm | Complexity Explorer_files/slack-kit-styles.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{LMhI:function(n,w,o){}},[["LMhI",2]]]);
---
+++ resources/chats/jvm-netlogo | Complexity Explorer_files/helper-styles.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{nF6r:function(n,w,o){}},[["nF6r",2]]]);
